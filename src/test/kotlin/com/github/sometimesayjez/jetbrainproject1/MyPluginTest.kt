32b73f4fc1c6241514fa614dc341ea26dd8ab3ef
sometimesayjez <stoleriu.cristina.02@gmail.com>
2023-12-15 12:50:09 +0100

    removed unnecessary tests

--- src/test/kotlin/com/github/sometimesayjez/jetbrainproject1/MyPluginTest.kt
+++ src/test/kotlin/com/github/sometimesayjez/jetbrainproject1/MyPluginTest.kt
@@ -9,23 +9,4 @@ import com.intellij.util.PsiErrorElementUtil
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
 
-    fun testXMLFile() {
-        val psiFile = myFixture.configureByText(XmlFileType.INSTANCE, "<foo>bar</foo>")
-        val xmlFile = assertInstanceOf(psiFile, XmlFile::class.java)
-
-        assertFalse(PsiErrorElementUtil.hasErrors(project, xmlFile.virtualFile))
-
-        assertNotNull(xmlFile.rootTag)
-
-        xmlFile.rootTag?.let {
-            assertEquals("foo", it.name)
-            assertEquals("bar", it.value.text)
-        }
-    }
-
-    fun testRename() {
-        myFixture.testRename("foo.xml", "foo_after.xml", "a2")
-    }
-
-    override fun getTestDataPath() = "src/test/testData/rename"
 }
